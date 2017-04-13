a57025ea69b33ad411cc921d25c2ec8742a6b9f6
oshou <adf1985adf@gmail.com>
2017-04-14 01:41:08 +0900

    modify adsense position

--- _includes/adsense.js
+++ _includes/adsense.js
@@ -1,9 +1,9 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- oshou.github.io -->
+<!-- oshou.github.io1 -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:600px"
+     style="display:inline-block;width:728px;height:90px"
      data-ad-client="ca-pub-2238031207667260"
-     data-ad-slot="5647561139"></ins>
+     data-ad-slot="6898478334"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
